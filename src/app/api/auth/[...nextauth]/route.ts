bc515f14a996d76cf0eb3bc1474e0fccaffa11d9
Ben Kinsey <bkinsey@gmail.com>
2023-07-14 20:47:29 -1000

    kv working

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
-import NextAuth, { AuthOptions, Session } from "next-auth";
+import { kv } from "@vercel/kv";
+import NextAuth, { AuthOptions } from "next-auth";
 
 import {
   nextAuthProviderList,
@@ -13,12 +14,28 @@ export const authOptions: AuthOptions = {
   providers,
   callbacks: {
     async signIn(userDetail) {
-      // console.log(userDetail);
       if (Object.keys(userDetail).length === 0) {
         return false;
       }
+
+      const email = userDetail?.user?.email;
+      const provider = userDetail?.account?.provider;
+      const providerAccountId = userDetail?.account?.providerAccountId;
+      const newLoginDate = new Date().toISOString();
+
+      if (!email || !provider) {
+        return false;
+      }
+
+      await kv.set(`${email}.lastLogin`, newLoginDate);
+      await kv.set(`${email}.providers.${provider}`, {
+        providerAccountId,
+        lastLogin: newLoginDate,
+      });
+
       return true;
     },
+
     async redirect({ baseUrl }) {
       return `${baseUrl}/protected`;
     },
--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -1,15 +1,45 @@
+import { kv } from "@vercel/kv";
+import { AuthOptions, Session } from "next-auth";
 import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
 
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import LogoutButton from "@/components/buttons/LogoutButton";
 
+interface UserData {
+  lastLogin: string;
+  providers: {
+    [provider: string]: {
+      providerAccountId?: string;
+      lastLogin?: string;
+    };
+  };
+}
+
+type CustomSession = Session & {
+  user: Session["user"] & {
+    provider?: string;
+    providerAccountId?: string;
+  };
+};
+
 export default async function Protected() {
-  const session = await getServerSession(authOptions);
+  const session = await getServerSession<AuthOptions, CustomSession>(
+    authOptions
+  );
 
   if (!session) {
     redirect("/");
   }
+
+  const email = session.user?.email;
+
+  if (!email) {
+    redirect("/");
+  }
+
+  const userData = await kv.get<UserData>(email);
+  console.log(JSON.stringify(userData, null, 2));
 
   return (
     <main className="max-w-2xl min-h-screen flex flex-col items-center mx-auto">
